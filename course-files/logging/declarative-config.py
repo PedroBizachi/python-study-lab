fc4f99421f891d2ccbc7da83c693c77601b0b87c
Pedro Bizachi <pedro_bizachi@outlook.com>
2026-06-12 19:42:25 -0300

    Structured Logging

--- course-files/logging/declarative-config.py
+++ course-files/logging/declarative-config.py
@@ -1,8 +1,10 @@
 # Declarative logging configuration - INI-file
 
+print("\nDeclarative logging configuration - INI-file")
+print("-----------------------\n")
+
 # Declarative logging configuration - Dictionary config
 
 # Declarative logging configuration - JSON config
 
 # Dynamically building config
-
