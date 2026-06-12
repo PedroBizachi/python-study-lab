340f45b2c5cf2bf4637c6503eabf1887338c6a1f
Pedro Bizachi <pedro_bizachi@outlook.com>
2026-06-12 20:28:35 -0300

    Declarative Configuration - INI File

--- course-files/logging/declarative-config.py
+++ course-files/logging/declarative-config.py
@@ -1,7 +1,23 @@
+import logging
+import logging.config
+"""
+# Uncomment to test INI configuration
+
 # Declarative logging configuration - INI-file
 
 print("\nDeclarative logging configuration - INI-file")
 print("-----------------------\n")
+
+config_path = "declarative-config.ini"
+
+logging.config.fileConfig(
+    fname=config_path,
+)
+
+app_logger = logging.getLogger("app")
+
+app_logger.debug("INI-style fileConfig is working!")
+"""
 
 # Declarative logging configuration - Dictionary config
 
